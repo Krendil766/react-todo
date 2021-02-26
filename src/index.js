de380e812db1ad2ce5fa72a8c9a993c41aa29e81
krendil766 <krendilek766@gmail.com>
2021-02-26 11:44:31 +0300

    added css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import TodoList from './components/todo-list'
+import AppHeader from './components/app-header/';
+import SearchPanel from './components/search-panel/';
+import TodoList from './components/todo-list/'
+import ItemStatusFilter from './components/item-status-filter'
 
+import './index.css'
 const App = () => {
 
   const todoData = [
@@ -18,9 +20,12 @@ const App = () => {
   ];
 
   return (
-    <div>
-      <AppHeader />
-      <SearchPanel />
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
       <TodoList todos={todoData} />
     </div>
   );
